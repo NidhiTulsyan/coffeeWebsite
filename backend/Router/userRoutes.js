e5b21ded04430d093233bb0a840dab08aab49506
Nidhi Tulsyan <tulsyannidhi19@outlook.com>
2023-10-03 00:12:09 +0530

    making code indented

--- backend/Router/userRoutes.js
+++ backend/Router/userRoutes.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import { login, signUp } from '../Controllers/userController.js';
+import express from "express";
+import { login, signUp } from "../Controllers/userController.js";
 const userRouter = express.Router();
 
-userRouter.post('/signup',signUp);
-userRouter.post('/login',login);
-userRouter.get('/',(req,res)=>{
-    res.json({"message":"hello"});
+userRouter.post("/signup", signUp);
+userRouter.post("/login", login);
+userRouter.get("/", (req, res) => {
+  res.json({ message: "hello" });
 });
 
 export default userRouter;
